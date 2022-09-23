1870d3b945b7d49545f2ed3ef855d5db176b01f8
karthii <karthikraj1439288@gmail.com>
2022-09-23 23:54:50 +0530

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SigIn_SignUp';
+  title = 'SigIn_SueignUp';
+  isSignIn = false;
+  Password_Type = "password"
+  Eye(){
+    if(this.Password_Type === 'password'){
+      this.Password_Type = 'text';
+    }
+    else if(this.Password_Type === 'text'){
+    this.Password_Type = 'password';
+    }
+  }
+  ToSignUp(){
+    this.isSignIn = false;
+  }
+  ToSignIn(){
+    this.isSignIn = true;
+  }   
 }
